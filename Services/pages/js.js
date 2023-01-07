6e2994ceae754a016de09f04242e0350a4d51cc6
ASHABA EMMANUEL <emmanuelashaba8@gmail.com>
2023-01-07 10:24:01 +0300

    javascript changes

---
+++ Services/pages/js.js
@@ -0,0 +1,156 @@
+function showContentA() {
+    let android = document.getElementById('android');
+    android.style.display='block';
+    document.querySelector(".one").style.fontSize='48px';
+    document.querySelector(".two").style.fontSize='25px';
+    document.querySelector(".three").style.fontSize='25px';
+    document.querySelector(".four").style.fontSize='25px';
+    document.querySelector(".five").style.fontSize='25px';
+    document.querySelector(".six").style.fontSize='25px';
+
+     apple = document.getElementById('apple');
+    apple.style.display='none';
+
+    support = document.getElementById('support');
+    support.style.display = 'none';
+
+    uiux = document.getElementById('ui/ux');
+    uiux.style.display = 'none';
+
+    reactnative = document.getElementById('reactnative');
+    reactnative.style.display = 'none';
+
+    webapps = document.getElementById('webapps');
+    webapps.style.display = 'none';  
+
+}
+function showContentB(){
+    let apple = document.getElementById('apple');
+    apple.style.display='block';
+    document.querySelector(".one").style.fontSize='25px';
+    document.querySelector(".two").style.fontSize='48px';
+    document.querySelector(".three").style.fontSize='25px';
+    document.querySelector(".four").style.fontSize='25px';
+    document.querySelector(".five").style.fontSize='25px';
+    document.querySelector(".six").style.fontSize='25px';
+
+    android = document.getElementById('android');
+    android.style.display='none';
+
+    support = document.getElementById('support');
+    support.style.display = 'none';
+
+    uiux = document.getElementById('ui/ux');
+    uiux.style.display = 'none';
+
+    reactnative = document.getElementById('reactnative');
+    reactnative.style.display = 'none';
+
+    webapps = document.getElementById('webapps');
+    webapps.style.display = 'none';  
+
+}
+function showContentC(){
+    let webapps = document.getElementById('webapps');
+    webapps.style.display = 'block';
+    document.querySelector(".one").style.fontSize='25px';
+    document.querySelector(".two").style.fontSize='25px';
+    document.querySelector(".three").style.fontSize='48px';
+    document.querySelector(".four").style.fontSize='25px';
+    document.querySelector(".five").style.fontSize='25px';
+    document.querySelector(".six").style.fontSize='25px';
+
+    apple = document.getElementById('apple');
+    apple.style.display='none';
+
+    support = document.getElementById('support');
+    support.style.display = 'none';
+
+    uiux = document.getElementById('ui/ux');
+    uiux.style.display = 'none';
+
+    reactnative = document.getElementById('reactnative');
+    reactnative.style.display = 'none';
+
+    android = document.getElementById('android');
+    android.style.display='none';
+
+
+}
+function showContentD(){
+    let reactnative = document.getElementById('reactnative');
+    reactnative.style.display = 'block';
+    document.querySelector(".one").style.fontSize='25px';
+    document.querySelector(".two").style.fontSize='25px';
+    document.querySelector(".three").style.fontSize='25px';
+    document.querySelector(".four").style.fontSize='48px';
+    document.querySelector(".five").style.fontSize='25px';
+    document.querySelector(".six").style.fontSize='25px';
+
+    android = document.getElementById('android');
+    android.style.display='none';
+
+    apple = document.getElementById('apple');
+    apple.style.display='none';
+
+    webapps = document.getElementById('webapps');
+    webapps.style.display = 'none';
+
+    support = document.getElementById('support');
+    support.style.display = 'none';
+
+    uiux = document.getElementById('ui/ux');
+    uiux.style.display = 'none';
+}
+function showContentE(){
+    let uiux = document.getElementById('ui/ux');
+    uiux.style.display = 'block';
+    
+    document.querySelector(".one").style.fontSize='25px';
+    document.querySelector(".two").style.fontSize='25px';
+    document.querySelector(".three").style.fontSize='25px';
+    document.querySelector(".four").style.fontSize='25px';
+    document.querySelector(".five").style.fontSize='48px';
+    document.querySelector(".six").style.fontSize='25px';
+
+    reactnative = document.getElementById('reactnative');
+    reactnative.style.display = 'none';
+
+    android = document.getElementById('android');
+    android.style.display='none';
+
+    apple = document.getElementById('apple');
+    apple.style.display='none';
+
+    webapps = document.getElementById('webapps');
+    webapps.style.display = 'none';  
+
+    support = document.getElementById('support');
+    support.style.display = 'none';
+}
+function showContentF(){
+    let support = document.getElementById('support');
+    support.style.display = 'block';
+    document.querySelector(".one").style.fontSize='25px';
+    document.querySelector(".two").style.fontSize='25px';
+    document.querySelector(".three").style.fontSize='25px';
+    document.querySelector(".four").style.fontSize='25px';
+    document.querySelector(".five").style.fontSize='25px';
+    document.querySelector(".six").style.fontSize='48px';
+
+    uiux = document.getElementById('ui/ux');
+    uiux.style.display = 'none';
+
+    reactnative = document.getElementById('reactnative');
+    reactnative.style.display = 'none';
+
+    android = document.getElementById('android');
+    android.style.display='none';
+
+    apple = document.getElementById('apple');
+    apple.style.display='none';
+
+    webapps = document.getElementById('webapps');
+    webapps.style.display = 'none';  
+
+}
